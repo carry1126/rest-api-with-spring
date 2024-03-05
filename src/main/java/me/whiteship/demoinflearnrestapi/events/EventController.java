b204d824a7f4b47d623c563cb79757ff69a1b21a
minjikim <carrykim1126@gmail.com>
2024-03-05 16:54:20 +0900

    reflect modify code

--- src/main/java/me/whiteship/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/whiteship/demoinflearnrestapi/events/EventController.java
@@ -1,9 +1,6 @@
 package me.whiteship.demoinflearnrestapi.events;
 
-import org.apache.tomcat.util.http.parser.MediaType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
